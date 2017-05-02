849effe6a9604f9f580c7c0ad64e27a15d2552d8
geigi <github@geigi.de>
2017-05-02 11:22:50 +0200

    pkgbuild, systemd service

---
+++ backlitkeyboardtimer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+UPDATE_INTERVAL=3
+TIMEOUT=30
+
+for i in "$@"
+do
+case $i in
+  -u=*|--update=*)
+  UPDATE_INTERVAL="${i#*=}"
+  shift
+  ;;
+  -t=*|--timeout=*)
+  TIMEOUT="${i#*=}"
+  shift
+  ;;
+  *)
+          # unknown option
+  ;;
+esac
+done
+
+brightness=$(cat /sys/class/leds/smc::kbd_backlight/brightness)
+restored=true
+
+while true
+do
+sleep $UPDATE_INTERVAL
+if [[ $(($TIMEOUT * 1000)) -lt $(xprintidle) ]]; then
+  if [[ restored ]]; then
+    brightness=$(cat /sys/class/leds/smc::kbd_backlight/brightness)
+    echo "0" > /sys/class/leds/smc::kbd_backlight/brightness
+    restored=false
+  fi
+elif [[ !restored ]]; then
+  restored=true
+  echo $brightness > /sys/class/leds/smc::kbd_backlight/brightness
+fi
+done
